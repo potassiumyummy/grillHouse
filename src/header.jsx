c5e1637026f65f7ffa5152a95c5573deefdec15e
jabrrtbrrt <cutypiecutie@gmail.com>
2026-08-29 17:19:33 +0800

    header udpate

--- src/header.jsx
+++ src/header.jsx
@@ -1,16 +1,21 @@
+import { useEffect, useState } from 'react'
 import './header.css';
 
-export default function Header(){
+
+
+const Header = () =>  {
     return(
         <div>
             <nav>
-                <ul className='flex gap-7.5 justify-end border border-solid pr-7.5'>
-                    <li>Home</li>
-                    <li>Products</li>
-                    <li>Services</li>
-                    <li>Contact Us</li>
+                <ul className='flex gap-7.5 justify-end pr-7.5 mt-2.5'>
+                    <li className='cursor-pointer'>Home</li>
+                    <li className='cursor-pointer'>Products</li>
+                    <li className='cursor-pointer'>Services</li>
+                    <li className='cursor-pointer'>Contact Us</li>
                 </ul>
             </nav>
         </div>
     );
 };
+
+export default Header
