81e6307168f79783e9082e254acadcb447fa33c9
jabrrtbrrt <cutypiecutie@gmail.com>
2026-08-29 18:04:22 +0800

    header.jsx update

--- src/header.jsx
+++ src/header.jsx
@@ -6,8 +6,8 @@ import './header.css';
 const Header = () =>  {
     return(
         <div>
-            <nav>
-                <ul className='flex gap-7.5 justify-end pr-7.5 mt-2.5'>
+            <nav className='bg-amber-500'>
+                <ul className='flex gap-7.5 justify-end pr-7.5 pb-2.5 pt-2.5 '>
                     <li className='cursor-pointer'>Home</li>
                     <li className='cursor-pointer'>Products</li>
                     <li className='cursor-pointer'>Services</li>
